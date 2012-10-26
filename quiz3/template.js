984a142f06a64b84f284a64a932a0edf1cd2d044
Zahra Jabini <zahra.arch@gmail.com>
2012-10-26 10:22:20 -0400

    quiz3

--- quiz3/template.js
+++ quiz3/template.js
@@ -1,7 +1,6 @@
 $(function(){
 
 $('#add').click(add_color)
-$('#box').on('hover', '.color', input_color);
 
 });
 
@@ -26,18 +25,6 @@ function add_color(){
 	var d = $('<div>'); 
 	d.addClass('color');
 	d.css('background-color', color); //now it has the style of the color that was inserted inside the text box
-	$('#colors').append(d)
+	$('#outputs').append(d)
 }
 
-
-function input_color()
-{
-	var input = $(this).css('background-color'); 
-	$('#input').css('background-color', input);
-}
-
-}
-
-function hovering(){
-	$(this).toggleClass('hover'); //this selects the thing you're on, so the box you're on AND YOU DO TOGGLE, SO WHEN YOU'RE ON IT, IT TURNS IT ON AND WHEN YOU'RE OFF OF IT IT TURNS IT OFF
-}
